e5d4da6d1b0bc2f2be5d3470d795b65624bce165
NewYeYeah <bitshaocheng@gmail.com>
2026-08-25 23:48:08 +0800

    feat: export multi-provider data interfaces

--- src/finagent/data/ingestion/__init__.py
+++ src/finagent/data/ingestion/__init__.py
@@ -1,3 +1,4 @@
+from .akshare import AKShareMarketDataIngestor
 from .alpaca import AlpacaMarketDataIngestor
 from .base import (
     MarketDataManifest,
@@ -10,18 +11,51 @@ from .base import (
     read_normalized_csv,
     validate_records,
 )
+from .diff import ProviderDiffReport, compare_provider_records
+from .hithink import HiThinkMarketDataIngestor, HiThinkRESTClient
+from .providers import (
+    DataCapability,
+    ProviderCapabilities,
+    ProviderDescriptor,
+    ProviderRegistry,
+    ProviderSymbolMap,
+    ProviderTier,
+    ResearchDataRequirement,
+)
+from .registry import (
+    ALPACA_CAPABILITIES,
+    TUSHARE_15K_CAPABILITIES,
+    create_provider,
+    default_provider_registry,
+)
 from .tushare import TushareMarketDataIngestor
 
 __all__ = [
+    "AKShareMarketDataIngestor",
+    "ALPACA_CAPABILITIES",
     "AlpacaMarketDataIngestor",
+    "DataCapability",
+    "HiThinkMarketDataIngestor",
+    "HiThinkRESTClient",
     "MarketDataManifest",
     "MarketDataPullRequest",
     "MarketDataQualityReport",
     "MarketRegion",
     "MaterializedMarketData",
     "NormalizedBarRecord",
+    "ProviderCapabilities",
+    "ProviderDescriptor",
+    "ProviderDiffReport",
+    "ProviderRegistry",
+    "ProviderSymbolMap",
+    "ProviderTier",
     "QualityIssue",
+    "ResearchDataRequirement",
+    "TUSHARE_15K_CAPABILITIES",
     "TushareMarketDataIngestor",
+    "compare_provider_records",
+    "create_provider",
+    "default_provider_registry",
     "read_normalized_csv",
     "validate_records",
 ]
