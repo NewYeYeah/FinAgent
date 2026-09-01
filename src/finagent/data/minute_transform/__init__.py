59d436d23c636ae5d717bbfb47fcbaefa8b7a40d
NewYeYeah <bitshaocheng@gmail.com>
2026-09-01 20:13:41 +0800

    data: export label materialization surface

--- src/finagent/data/minute_transform/__init__.py
+++ src/finagent/data/minute_transform/__init__.py
@@ -1,3 +1,12 @@
+from .labeling import (
+    CANONICAL_US_60M_LABEL_NAME,
+    LabelMaterializationSpec,
+    LabelQueryPlan,
+    LabelSeriesEvidence,
+    SameSessionLabelStore,
+    build_same_session_label_plan,
+    canonical_same_session_60m_label_spec,
+)
 from .resample import (
     RESAMPLED_MINUTE_ADAPTER_ID,
     ResamplingEvidence,
@@ -15,15 +24,22 @@ from .sessionize import (
 )
 
 __all__ = [
+    "CANONICAL_US_60M_LABEL_NAME",
     "RESAMPLED_MINUTE_ADAPTER_ID",
     "SESSIONIZED_MINUTE_ADAPTER_ID",
     "CalendarSessionizedMinuteStore",
+    "LabelMaterializationSpec",
+    "LabelQueryPlan",
+    "LabelSeriesEvidence",
     "ResamplingEvidence",
     "ResamplingSpec",
+    "SameSessionLabelStore",
     "SessionResampledMinuteStore",
     "SessionizationEvidence",
     "SessionizationSpec",
     "build_resampled_minute_plan",
+    "build_same_session_label_plan",
     "build_sessionized_minute_plan",
+    "canonical_same_session_60m_label_spec",
     "load_trading_calendar_evidence_json",
 ]
