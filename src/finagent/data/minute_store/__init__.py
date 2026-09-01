5d71feb1edcfa7adbdd7dd7d17fe43c7a9786e24
NewYeYeah <bitshaocheng@gmail.com>
2026-09-01 18:03:21 +0800

    style: sort minute store exports

--- src/finagent/data/minute_store/__init__.py
+++ src/finagent/data/minute_store/__init__.py
@@ -10,16 +10,16 @@ from .materialize import (
     count_plan_rows,
     fetch_plan_rows,
 )
-from .parquet_store import DuckDBParquetMinuteStore, US_MINUTE_DUCKDB_ADAPTER_ID
+from .parquet_store import US_MINUTE_DUCKDB_ADAPTER_ID, DuckDBParquetMinuteStore
 from .query import MinuteQueryPlan, build_minute_query_plan, select_partitions
 
 __all__ = [
+    "US_MINUTE_DUCKDB_ADAPTER_ID",
     "DuckDBParquetMinuteStore",
     "MinuteMaterialization",
     "MinuteQueryPlan",
     "MinuteStoreManifest",
     "MinuteStorePartition",
-    "US_MINUTE_DUCKDB_ADAPTER_ID",
     "build_minute_query_plan",
     "copy_plan_to_parquet",
     "count_plan_rows",
