f71c315770af5d61718d3e8fc20af21a3250be2b
NewYeYeah <bitshaocheng@gmail.com>
2026-09-01 18:09:50 +0800

    data: export bounded execution contracts

--- src/finagent/data/minute_store/__init__.py
+++ src/finagent/data/minute_store/__init__.py
@@ -1,3 +1,9 @@
+from .execution import (
+    DEFAULT_DUCKDB_EXECUTION_POLICY,
+    DuckDBExecutionPolicy,
+    DuckDBExecutionSettings,
+    configure_duckdb_connection,
+)
 from .manifest import (
     MinuteStoreManifest,
     MinuteStorePartition,
@@ -9,21 +15,27 @@ from .materialize import (
     copy_plan_to_parquet,
     count_plan_rows,
     fetch_plan_rows,
+    inspect_execution_settings,
 )
 from .parquet_store import US_MINUTE_DUCKDB_ADAPTER_ID, DuckDBParquetMinuteStore
 from .query import MinuteQueryPlan, build_minute_query_plan, select_partitions
 
 __all__ = [
+    "DEFAULT_DUCKDB_EXECUTION_POLICY",
     "US_MINUTE_DUCKDB_ADAPTER_ID",
+    "DuckDBExecutionPolicy",
+    "DuckDBExecutionSettings",
     "DuckDBParquetMinuteStore",
     "MinuteMaterialization",
     "MinuteQueryPlan",
     "MinuteStoreManifest",
     "MinuteStorePartition",
     "build_minute_query_plan",
+    "configure_duckdb_connection",
     "copy_plan_to_parquet",
     "count_plan_rows",
     "fetch_plan_rows",
+    "inspect_execution_settings",
     "manifest_from_directory",
     "manifest_from_huggingface_snapshot",
     "select_partitions",
